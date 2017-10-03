9227f601e40668e78bcb9438bb4d787a2944a515
ALUMNADO\joalga01 <josebeniparrell96@gmail.com>
2017-10-03 12:32:09 +0200

    adivinacion

---
+++ adivinacion.js
@@ -0,0 +1,13 @@
+var numero2=23;
+var numero1=prompt("Escribe un numero:");
+
+
+if (numero1<numero2) {
+  alert("Te quedaste corto")
+}
+else if(numero1>numero2){
+alert("Te pasaste")
+}
+else if(numero1=numero2){
+alert("Acertaste")
+}
